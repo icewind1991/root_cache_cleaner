66a9d8ce4a0ef465f3d2c86ba057f5d47776293b
Robin Appelman <robin@icewind.nl>
2021-08-17 17:59:11 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,26 @@
+# Root cache cleaner
+
+Removes duplicate filecache entry from the root filecache.
+
+## Why
+
+In certain circumstances it is possible that Nextcloud creates duplicate items in the filesystem cache,
+adding the items of a user both to the per-user cache and the global root cache.
+
+While these duplicates generally don't cause any issues they can bloat the filecache significantly and lead
+to degraded performance.
+
+## What
+
+This app can clean the duplicate filecache entries by going through each user and looking for cache items in the root
+cache that should be in the per-user cache instead, deleting any it finds.
+
+While these steps should be perfectly safe, an abundance of caution should always be taking with these kinds of
+large database deletions, and it is **strongly** recommended to ensure that proper database backup is in place before
+running the cleanup.
+
+## How
+
+- Ensure proper database backends are in place.
+- Install the app.
+- Run the cleaner with `occ root_cache_cleaner:clean`.
--- lib/Command/Cleanup.php
+++ lib/Command/Cleanup.php
@@ -24,29 +24,69 @@ declare(strict_types=1);
 namespace OCA\RootCacheClean\Command;
 
 use OC\Core\Command\Base;
+use OCP\Files\IRootFolder;
 use OCP\IDBConnection;
+use OCP\IUserManager;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class Cleanup extends Base {
 	/** @var IDBConnection */
 	private $connection;
+	/** @var IUserManager */
+	private $userManager;
+	/** @var IRootFolder */
+	private $rootFolder;
 
-	public function __construct(IDBConnection $connection) {
+	public function __construct(IDBConnection $connection, IUserManager $userManager, IRootFolder $rootFolder) {
 		parent::__construct();
 		$this->connection = $connection;
+		$this->userManager = $userManager;
+		$this->rootFolder = $rootFolder;
 	}
 
 	protected function configure() {
 		$this
 			->setName('root_cache_cleaner:clean')
 			->setDescription('Clean duplicate items from the root filecache')
-			->addOption('no-backup', null, InputOption::VALUE_NONE, "Disable the creation of a backup file");
+			->addOption('no-warning', null, InputOption::VALUE_NONE, "Disable the warning before starting the cleaning process");
 		parent::configure();
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$rootStorageId = $this->rootFolder->get('')->getStorage()->getCache()->getNumericStorageId();
+
+		if (!$input->getOption('no-warning')) {
+			$helper = $this->getHelper('question');
+			$output->writeln("While the cleanup process should be safe there is still a risk involved in bulk deleting filecache entries like this.");
+			$output->writeln("It is <options=underscore>strongly</> recommended to ensure that a proper database backup is in place before running this process.");
+			$question = new ConfirmationQuestion("Continue? [y/N]", false);
+
+			if (!$helper->ask($input, $output, $question)) {
+				return 0;
+			}
+		}
+
+		$deleted = 0;
+		$this->userManager->callForSeenUsers(function ($user) use ($rootStorageId, &$deleted, $output) {
+			$output->write("Cleaning for user <info>" . $user->getUID() . "</info>...");
+			$userDeleted = $this->cleanForUser($rootStorageId, $user->getUID());;
+			$output->writeln(" Done, deleted <info>$userDeleted</info> entries.");
+			$deleted += $userDeleted;
+		});
+
+		$output->writeln("Deleted <info>$deleted</info> entries in total.");
+
 		return 0;
+	}
+
+	protected function cleanForUser(int $rootStorageId, string $uid): int {
+		$query = $this->connection->getQueryBuilder();
+		$query->delete('filecache')
+			->where($query->expr()->eq('storage', $query->createNamedParameter($rootStorageId)))
+			->andWhere($query->expr()->like('path', $query->createNamedParameter($this->connection->escapeLikeParameter("$uid/") . '%')));
+		return $query->execute();
 	}
 }
